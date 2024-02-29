729e04518a5b789b8ed40772fcb6a2df3ba2a5ce
marikamezawa <159060219+marikamezawa@users.noreply.github.com>
2024-02-29 11:41:12 -0300

    adicionando cena 1

---
+++ Cena01.js
@@ -0,0 +1,18 @@
+class Cena01 extends Phaser.Scene {
+    constructor() {
+        super('Cena01');
+    }
+
+    create() {
+        // Serve para exibir o overlay de entrada
+        document.getElementById('inputOverlay').style.display = 'block';
+
+        // Configura o evento de clique no botão
+        document.getElementById('startButton').addEventListener('click', () => {
+            const playerName = document.getElementById('nameInput').value;
+            this.scene.start('Cena02', { playerName: playerName });
+            // Oculta o overlay após iniciar
+            document.getElementById('inputOverlay').style.display = 'none';
+        });
+    }
+}
